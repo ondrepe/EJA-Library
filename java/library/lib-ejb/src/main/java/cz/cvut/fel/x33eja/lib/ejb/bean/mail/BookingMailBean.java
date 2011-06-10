ca167a1d6fe4ff1f66d604b4e63cf1fab1ea681d
Simon Stastny <stastny.simon@gmail.com>
2011-06-10 22:11:38 +0200

    finish

--- java/library/lib-ejb/src/main/java/cz/cvut/fel/x33eja/lib/ejb/bean/mail/BookingMailBean.java
+++ java/library/lib-ejb/src/main/java/cz/cvut/fel/x33eja/lib/ejb/bean/mail/BookingMailBean.java
@@ -38,33 +38,20 @@ public class BookingMailBean implements MessageListener {
     MailSenderCommand command = new MailSenderCommand(mailSession);
     command.sendEmail(notification, createMessageText(notification));
   }
-  
+
   private String createMessageText(MailObject notification) {
     StringBuilder builder = new StringBuilder();
     builder.append("<html lang=\"en\"><head>").append(LineSeparator.Web);
     builder.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />").append(LineSeparator.Web);
     builder.append("<title>Blockatoion</title><s/head><body>").append(LineSeparator.Web);
-    builder.append("<table><tr><td>")
-            .append("Reader: ").append("</td><td>")
-            .append(notification.getName())
-            .append(" ")
-            .append(notification.getSurname())
-            .append("</td><td>").append(notification.getReaderEmail())
-            .append("</td></tr></table>")
-            .append(LineSeparator.Web);
+    builder.append("<table><tr><td>").append("Reader: ").append("</td><td>").append(notification.getName()).append(" ").append(notification.getSurname()).append("</td><td>").append(notification.getReaderEmail()).append("</td></tr></table>").append(LineSeparator.Web);
     builder.append("<br />").append(LineSeparator.Web);
     builder.append("<p>Blocked books: </p>").append(LineSeparator.Web);
     builder.append("<table>");
-    builder.append("<thead><tr>")
-            .append("<th>Id</th>")         
-            .append("<th>Book</th>")
-            .append("<th>From date</th>")
-            .append("<th>To date</th>") 
-            .append("</tr></thead>")
-            .append(LineSeparator.Web);
+    builder.append("<thead><tr>").append("<th>Id</th>").append("<th>Book</th>").append("<th>From date</th>").append("<th>To date</th>").append("</tr></thead>").append(LineSeparator.Web);
     builder.append("<tbody>").append(LineSeparator.Web);
-    
-    for(ChargeOut chr : notification.getChargeOuts()) {
+
+    for (ChargeOut chr : notification.getChargeOuts()) {
       builder.append("<tr>");
       builder.append("<td>").append(chr.getId()).append("</td>");
       builder.append("<td>").append(chr.getBook().getName()).append(" (").append(chr.getBook().getYear()).append(")</td>");
@@ -72,7 +59,7 @@ public class BookingMailBean implements MessageListener {
       builder.append("<td>").append(chr.getTo()).append("</td>");
       builder.append("</tr>").append(LineSeparator.Web);
     }
-    
+
     builder.append("</tbody></table>").append(LineSeparator.Web);
     builder.append("</body>").append("</html>");
     return builder.toString();
